3ce12f0496951bd5044d2bcef58b27ffca60f18b
cameron1m <45009513+cameron1m@users.noreply.github.com>
2020-07-23 01:16:42 -0700

    added node.js with socket and express

---
+++ app.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+let serv = require('http').Server(app);
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + "/client/mlProject.html");
+});
+
+app.use('/client', express.static(__dirname + '/client'));
+
+serv.listen(2000);
+console.log("Server Started");
+
---
+++ test.py
@@ -0,0 +1,6 @@
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+keyboard.type('Hello World')
